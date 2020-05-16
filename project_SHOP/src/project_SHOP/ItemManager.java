05030546776551df4dd4e49fcc3ab9b101a85552
ahrimy <dlsdjdkfla@gmail.com>
2020-05-17 02:28:05 +0900

    #Stored Items
    Stored items, amount of each items

---
+++ project_SHOP/src/project_SHOP/ItemManager.java
@@ -0,0 +1,154 @@
+package project_SHOP;
+
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ItemManager {
+	HashMap<Item, Integer> itemList = new HashMap<>();
+	Scanner scan = new Scanner(System.in);
+
+	public Item search(String name) {
+		Item check = null;
+		for (Item key : itemList.keySet()) {
+			if (key.getName().equals(name)) {
+				check = key;
+				break;
+			}
+		}
+		return check;
+	}
+
+	public void addItem() {
+		System.out.print("Enter the item name : ");
+		String name = scan.next();
+		Item check = search(name);
+		if (check == null) {
+			System.out.print("Enter the item price : ");
+			int price = scan.nextInt();
+			System.out.print("Enter the number of items: ");
+			int cnt = scan.nextInt();
+
+			Random ran = new Random();
+			int c = 0;
+			int num = 0;
+			while (true) {
+				c = 0;
+				num = ran.nextInt(9) + 1;
+				for (int i = 0; i < 5; i++) {
+					num *= 10;
+					num += ran.nextInt(10);
+				}
+				for (Item key : itemList.keySet()) {
+					if (key.getItemNum() == num) {
+						c = 1;
+						break;
+					}
+				}
+				if (c == 0) {
+					break;
+				}
+			}
+			itemList.put(new Item(name, price, num), cnt);
+		} else {
+			System.out.println(name + " is already on the Item List");
+		}
+	}
+
+	public void changeItem() {
+		System.out.print("Enter the item name : ");
+		String name = scan.next();
+		Item check = search(name);
+		if (check != null) {
+			while (true) {
+				printStoredItem();
+				System.out.println();
+				System.out.println("[1]change amount");
+				System.out.println("[2]update name");
+				System.out.println("[3]update price");
+				System.out.println("[0]Back");
+				int sel = scan.nextInt();
+				if (sel == 1) {
+					System.out.println();
+					System.out.println("Current : " + check.getName() + " - " + itemList.get(check));
+					System.out.print("Change Amount(+/-): ");
+					int cnt = scan.nextInt();
+					updateItem(check, cnt);
+
+				} else if (sel == 2) {
+					System.out.println("Enter the new name : ");
+					String inputName = scan.next();
+					check.setName(inputName);
+				} else if (sel == 3) {
+					System.out.println("Enter the new price : ");
+					int inputPrice = scan.nextInt();
+					check.setPrice(inputPrice);
+
+				} else if (sel == 0) {
+					printStoredItem();
+					break;
+				}
+
+			}
+		} else {
+			System.out.println("This item is not on the list");
+		}
+	}
+
+	public void updateItem(Item item, int cnt) {
+		int current = itemList.get(item);
+		itemList.replace(item, current + cnt);
+	}
+
+	public void deleteItem() {
+		System.out.print("Enter the item name : ");
+		String name = scan.next();
+		Item check = search(name);
+		if (check != null) {
+			itemList.remove(check);
+		} else {
+			System.out.println(name + " is not on the Item List");
+		}
+	}
+
+	public void printItems() {
+		if (itemList.isEmpty()) {
+			System.out.println();
+			System.out.println("Item Storage is empty.");
+		} else {
+			for (Item key : itemList.keySet()) {
+				key.print();
+				System.out.println(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+	public void printStoredItem() {
+		if (itemList.isEmpty()) {
+			System.out.println();
+			System.out.println("Item Storage is empty.");
+		} else {
+			for (Item key : itemList.keySet()) {
+				key.print();
+				System.out.println(" : " + itemList.get(key));
+			}
+			System.out.println();
+		}
+	}
+
+	public void printCnts() {
+		if (itemList.isEmpty()) {
+			System.out.println();
+			System.out.println("Item Storage is empty.");
+		} else {
+			for (Item key : itemList.keySet()) {
+				key.printName();
+				System.out.println(" : " + itemList.get(key));
+
+			}
+			System.out.println();
+		}
+	}
+}
